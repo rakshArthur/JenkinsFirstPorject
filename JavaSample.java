0dcc047c80818d92d6ea9665ede4a754d7e838c5
rakshArthur <62903019+rakshArthur@users.noreply.github.com>
2020-03-31 23:44:28 +0530

    success build from SCM

--- JavaSample.java
+++ JavaSample.java
@@ -1,6 +1,6 @@
 public class JavaSample {
 	
 	public static void main(String args[]){
-		System.out.println("First Jenkins Job with changes to trigger 3rd build");
+		System.out.println("First Jenkins Job with changes to trigger success build from SCM");
 	}
 }
