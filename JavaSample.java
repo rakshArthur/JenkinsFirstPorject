94c7ebde6ed31769dc275a8d35b4412455ce7350
rakshArthur <62903019+rakshArthur@users.noreply.github.com>
2020-03-31 23:36:26 +0530

    changes to trigger 2nd build

--- JavaSample.java
+++ JavaSample.java
@@ -1,6 +1,6 @@
 public class JavaSample {
 	
 	public static void main(String args[]){
-		System.out.println("First Jenkins Job with changes trigger build");
+		System.out.println("First Jenkins Job with changes to trigger 2nd build");
 	}
 }
