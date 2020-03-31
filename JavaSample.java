3ee82aaa2aa7a8d966892f008c431c07d565ddf1
rakshArthur <62903019+rakshArthur@users.noreply.github.com>
2020-03-31 15:40:20 +0530

    added changes to trigger build

--- JavaSample.java
+++ JavaSample.java
@@ -1,6 +1,6 @@
 public class JavaSample {
 	
 	public static void main(String args[]){
-		System.out.println("First Jenkins Job");
+		System.out.println("First Jenkins Job with changes trigger build");
 	}
 }
