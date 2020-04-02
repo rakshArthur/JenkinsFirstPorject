13829b87dd2665f45f91ca379bfd40e328ec870f
rakshArthur <62903019+rakshArthur@users.noreply.github.com>
2020-04-02 09:23:44 +0530

    using Webhook!

--- JavaSample.java
+++ JavaSample.java
@@ -1,6 +1,6 @@
 public class JavaSample {
 	
 	public static void main(String args[]){
-		System.out.println("First Jenkins Job with changes to trigger success build from SCM");
+		System.out.println("First Jenkins Job trying to trigger build using Webhook!!");
 	}
 }
